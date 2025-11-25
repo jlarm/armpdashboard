c5dd4aa44f8295de5918c4f212b45490034ac659
jlarm <117611704+jlarm@users.noreply.github.com>
2025-11-25 11:15:45 -0600

    Update env example

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 final class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        User::query()->create([
+            'name' => 'Joe Lohr',
+            'email' => 'jlohr@autorisknow.com',
+            'password' => bcrypt('password'),
+            'role' => Role::ADMIN,
+        ]);
     }
 }
